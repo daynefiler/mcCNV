eafaf878a9cd532b8319e753efa797f0a5823f5d
Dayne <dayne.filer@gmail.com>
2020-07-06 10:11:41 -0400

    Removed cigar2rlen
    The actual breakpoints were not used in the end, and
    the function as it stands here is not working (not
    sure what changed). For example cigar2rlen("99M") returned 0.
    Using isize should suffice for now.

--- src/init.c
+++ src/init.c
@@ -2,11 +2,6 @@
 #include <R_ext/Rdynload.h>
 #include "mcCNV.h"
 
-static const R_CMethodDef cMethods[] = {
-  {"cigar2rlen", (DL_FUNC) &cigar2rlen, 3},
-  {NULL, NULL, 0}
-};
-
 static const R_CallMethodDef callMethods[] = {
   {"calcMLCN", (DL_FUNC) &calcMLCN, 5},
   {NULL, NULL, 0}
@@ -14,7 +9,7 @@ static const R_CallMethodDef callMethods[] = {
 
 void R_init_mcCNV(DllInfo *dll)
 {
-    R_registerRoutines(dll, cMethods, callMethods, NULL, NULL);
+    R_registerRoutines(dll, NULL, callMethods, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
     R_forceSymbols(dll, TRUE);
 }
--- src/mcCNV.h
+++ src/mcCNV.h
@@ -1,5 +1,3 @@
 #include "Rdefines.h"
 
-void cigar2rlen(char **cs, int *n, int *result);
-
 SEXP calcMLCN(SEXP N, SEXP mu, SEXP sf, SEXP phi, SEXP prior);
